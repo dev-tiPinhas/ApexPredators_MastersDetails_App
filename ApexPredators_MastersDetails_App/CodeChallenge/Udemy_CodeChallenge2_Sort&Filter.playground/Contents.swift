7094b48539ade33c3f4f2cc4aebe5b1368a26f99
Tiago Pinheiro <tiago.pinheiro@itsector.pt>
2023-10-25 21:37:16 +0100

    +add - CodeChallenge;

---
+++ ApexPredators_MastersDetails_App/CodeChallenge/Udemy_CodeChallenge2_Sort&Filter.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// Given an array of non-empty String values, remove all the strings that start with a vowel (a, e, i, o, u), regardless of case, and sort the remaining strings, again regardless of case, by descending alphabetical order (z to a).
+
+// Return whatever is left in the filtered and sorted array.
+
+// - Example 1:
+//
+// Given array: ["Alan", "Timothy", "Kevin", "Ethan", "Matthew"]
+//
+// Filtered array: ["Timothy", "Kevin", "Matthew"]
+//
+// Sorted array (final result): ["Timothy", "Matthew", "Kevin"]
+
+// - Example 2:
+//
+// Given array: ["lion", "tiger", "bear", "eagle", "Big Bird", "raccoon", "skunk", "Toothless", "aardvark", "baboon", "Old Yeller"]
+//
+// Filtered array: ["lion", "tiger", "bear", "Big Bird", "raccoon", "skunk", "Toothless", "baboon"]
+//
+// Sorted array (final result): ["Toothless", "tiger", "skunk", "raccoon", "lion", "Big Bird", "bear", "baboon"]
+
+func sortAndFilter(_ stringArray: [String]) -> [String] {
+    //let temp = stringArray.filter { $0. }
+    
+    return []
+}
