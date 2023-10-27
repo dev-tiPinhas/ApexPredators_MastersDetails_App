8fcbc378dc4088f0c0e6230cc51f9478fa2fd73d
Tiago Pinheiro <tiago.pinheiro@itsector.pt>
2023-10-27 19:39:02 +0100

    *Change - improve code Challenge;

--- ApexPredators_MastersDetails_App/CodeChallenge/Udemy_CodeChallenge2_Sort&Filter.playground/Contents.swift
+++ ApexPredators_MastersDetails_App/CodeChallenge/Udemy_CodeChallenge2_Sort&Filter.playground/Contents.swift
@@ -20,32 +20,14 @@ import UIKit
 //
 // Sorted array (final result): ["Toothless", "tiger", "skunk", "raccoon", "lion", "Big Bird", "bear", "baboon"]
 
-func validateStringFirstElement(string: String) -> [String] {
-    var tempList: [String] = []
-    
-    if let firstChar = string.first,
-       firstChar.lowercased() != "a",
-       firstChar.lowercased() != "e",
-       firstChar.lowercased() != "i",
-       firstChar.lowercased() != "o",
-       firstChar.lowercased() != "u" {
-        tempList.append(string)
-    }
-    
-    return tempList
-}
-
 func sortAndFilter(_ stringArray: [String]) -> [String] {
-    var list: [String] = []
-    stringArray.forEach { string in
-        list.append(contentsOf: validateStringFirstElement(string: string))
+    let setInvalidChars: Set<String> = ["a", "e", "i", "o", "u"]
+    
+    let filteredList = stringArray.filter {
+        !setInvalidChars.contains($0.first?.lowercased() ?? "")
     }
     
-    let sortedList = list.sorted { (lhs: String, rhs: String) -> Bool in
-        return lhs.caseInsensitiveCompare(rhs) == .orderedDescending
-    }
-    
-    return sortedList //list.sorted(by: { $0 > $1 } ) // does not work because of case insensitiveCompare
+    return filteredList.sorted(by: { $0.lowercased() > $1.lowercased() } )
 }
 
 
